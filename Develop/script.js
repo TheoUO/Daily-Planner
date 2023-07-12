42346d98182793562f21d4bd3571a722b92d9e59
TCodes <theodoreuokeke@gmail.com>
2023-07-12 06:01:49 +0100

    added localstorage getItem

--- Develop/script.js
+++ Develop/script.js
@@ -11,10 +11,10 @@ $('#currentDay').text(today);
 
 
 
-//Second
+
 // Add additional time blocks to HTML
 
-//Third
+
 // Color code time blocks to indicate Past / Present / Future
    // Detect past / present / future
 
@@ -41,7 +41,7 @@ function blockColor() {
    }
    blockColor()
 
-// Fourth
+
 // Click the time-block, be able to input information
 // When I click the save button - text is saved in local storage 
 // Event Listener on Save Button to save the info to Local Storage
@@ -56,26 +56,17 @@ $('.Btn').on('click', function() {
 saveText()
 
 
-//Fifth 
+
 // When we refresh the page, then the saved events persist.
 
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+
+
+
+
+$('time-block').each(function() {
+  var key = $(this).parent().attr('id')
+  var value = localStorage.getItem(key);
+  $(this).children('.description').val(value);
+
 });
+
