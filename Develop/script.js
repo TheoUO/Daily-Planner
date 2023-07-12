c257caf3a18c5dc046b528c7d79556c1f035527d
TCodes <theodoreuokeke@gmail.com>
2023-07-12 03:28:50 +0100

    added time-blocks to html and added to JS

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,51 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+
+// Add current date to dayjs to display in header
+//var currentDate = $('currentDay');
+//$('#currentDay').text(today.format('MMM D, YYYY'));
+
+var today = dayjs().format('ddd, MMM D, YYYY');
+$('#currentDay').text(today);
+
+
+
+//Second
+// Add additional time blocks to HTML
+
+//Third
+// Color code time blocks to indicate Past / Present / Future
+   // Detect past / present / future
+
+var now = dayjs().format('H'); // = 18 
+   console.log(now);
+
+
+
+//var timeBlock = $('.time-block')
+
+//assign each time block a value to compare to now
+
+function blockColor() {
+   $('.time-block').each(function() {
+    var hour = parseInt(this.id);
+    $(this).toggleClass('past', hour < now);
+    $(this).toggleClass('present', hour === now);
+    $(this).toggleClass('future', hour > now);
+
+   });
+
+   }
+
+// Fourth
+// Click the time-block, be able to input information
+// When I click the save button - text is saved in local storage 
+// Event Listener on Save Button to save the info to Local Storage
+
+//Fifth 
+// When we refresh the page, then the saved events persist.
+
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
